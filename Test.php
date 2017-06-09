4441ec4c4afc1aae0c0a36c8ded39bd2c306af22
qwertyfire <c.w.walmsley@gmail.com>
2017-06-09 10:59:46 +1000

    adding extra pictures etc

---
+++ Test.php
@@ -0,0 +1,88 @@
+<html>
+    <head>
+        <style type="text/css">
+            img{border:0;clear:both;display:block;float:none;height:auto !important;margin: 3px 10px 3px 0;max-width:100% !important}
+            object, embed{border:0;clear:both;display:block;float:none}
+            a img{text-decoration:none}
+            p{clear:both}
+            .content * {font-family:'AvenirNext-Regular', Avenir, 'Helvetica Neue', Arial, Helvetica, sans-serif;font-size:18px;line-height:1.4;color:#333 !important;}
+            font {line-height: inherit !important; font-size: inherit !important}
+            a, a:visited, .content a {color:#09f !important;text-decoration:none !important}
+            h1 a {color: white !important}
+
+            h1 {font-size:36px !important}
+            h2 {font-size:27px !important}
+            h3 {font-size:21px !important}
+            h4 {font-size:18px !important}
+            h5 {font-size:18px !important}
+            h6 {font-size:18px !important}
+            h1, h2, h3 { line-height: 1.125 !important }
+            h4, h5, h6 { line-height: 1.25 !important }
+
+
+            @media screen and (max-width:700px) {
+                .content {
+                    width: 100%!important;
+                }
+            }
+        </style>
+    </head>
+
+    <body>
+        <table cellpadding="0" cellspacing="0" border="0" style="padding:0px;margin:0px;text-decoration:none;" width="100%">
+            <tr>
+                <td style="padding:0px;margin:0px;">
+                    <table class="content" cellpadding="0" cellspacing="0" border="0" width="600">
+                        <tr>
+                            <td>
+                                <table style="border-collapse:collapse;width:100%;max-width:100%" cellpadding="0" cellspacing="0">
+                                    <tr>
+                                        <td>
+                                            <b>Test</b><br>
+                                            From Christopher
+                                            Walmsley see 61413684948<br><br>
+                                            June 09, 2017 at 08:45AM<br>
+                                            via Android
+                                        </td>
+                                    </tr>
+                                </table>
+
+
+
+
+
+                                <a href="https://links.ifttt.com/wf/click?upn=jGHDfwvrKDxaofcg5GVulqZbq5Ob51WMw-2B5VP4So20lMezuB5ALm9NFE9oUFH6n5XihgssYthB0Vrh2s0C1RKA-3D-3D_-2BPPYzd87qq10IXJQDD0DUz4r3z8HBF8P-2BdEwmfo4WKyupP-2FbcggiSb5xMfb06nQwJwyUYjs5TqbCrnCU0U-2BWs-2FZwpWz-2Bdn9At3jVT0HciQaUANbU9-2BE8hok-2Fc9GYGVVPQrjbOntL51AbK8DRbdr-2FDVsuPc-2BtP4ZNGjtdrtWBpQvWZF6-2BjV0nVzklv97UrCKENRPTLRkZjZPYBiYjtNrDsTa8zzS-2FQlIggs-2FMqEdfNAUlK2s2326tkSGnZCBgSPCkaNrcGfd5jAefgZyXU2LpkrnORzZpGCGtd-2B43zvX4X-2BeF5n0e7F03zeJS29sYDhvwoLkKzNLoPxKvbgquyobS-2FA-3D-3D">
+                                    <table cellpadding="12" cellspacing="0" style="width:100%;margin-top:36px;border-radius:10px;background-color: #1D9A59;">
+                                        <tr>
+                                            <td width="52" align="right">
+                                                <img width="40" style="margin:0;border:0" src="https://assets.ifttt.com/images/channels/1322033008/icons/on_color_regular.png" title="Android SMS" alt="Android SMS" />
+                                            </td>
+                                            <td>
+                                                <h3 style="margin: 6px auto;">
+                                                    <font style="color: white !important;">
+                                                    Automatically have the texts you receive on your Android phone sent to your email
+                                                    </font>
+                                                </h3>
+                                            </td>
+                                            <td width="22" align="left">
+                                                <img width="10" border="0" valign="middle" align="right" style="vertical-align:middle;border:0;border-style:none;margin:0;padding:0;margin-right:6px;" src="https://web-assets.ifttt.com/images/email-chevron-right.png" />
+                                            </td>
+                                        </tr>
+                                    </table>
+                                </a>
+
+
+
+                                <a href="https://links.ifttt.com/wf/click?upn=6tNAOFwxcF8KzWlMrDEXkMBsNv6tbjg3LZSM0iBW7bQ-3D_-2BPPYzd87qq10IXJQDD0DUz4r3z8HBF8P-2BdEwmfo4WKyupP-2FbcggiSb5xMfb06nQwJwyUYjs5TqbCrnCU0U-2BWs-2FZwpWz-2Bdn9At3jVT0HciQaUANbU9-2BE8hok-2Fc9GYGVVPlkMr8EYhJi5dPGufqWhpqaiviof-2FJaPZ4fSjkK2-2B-2FHEs2OUQcUt-2FmhckDYIAeVDpAdtzUMYNddCv3eFkb0-2BzhuDvusofmzj70ubpItjp12aFJkCvNZ3xp2tqcolwAyAi5RaOs6aKM0TXpCQN3P-2BFqNH8t0Bkx8sSPiftlZtuLSUndrWbrsZa4ND-2BRkHC0Rm7zs5s5Pb62EuZUI73pQooKw-3D-3D">
+                                    <img width="60" src="https://web-assets.ifttt.com/images/email_logo-lighter.png" alt="IFTTT" title="IFTTT" style="display:inline; border: 0;margin-top: 24px;"/>
+                                </a>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+        </table>
+
+        <img src="https://links.ifttt.com/wf/open?upn=-2BPPYzd87qq10IXJQDD0DUz4r3z8HBF8P-2BdEwmfo4WKyupP-2FbcggiSb5xMfb06nQwJwyUYjs5TqbCrnCU0U-2BWs-2FZwpWz-2Bdn9At3jVT0HciQaUANbU9-2BE8hok-2Fc9GYGVVPY1IxQnxHJbeOdSqg0MGq5TglJYIS6wNFiliFVRGxYW1RVuqC1l1wTauxbblcw4b6uSdH32F7UDc9EqWij-2F5QOBZ-2FQqLGGRAYcLHRseLYiqf-2Fq1XV-2BYS9IqrysnVMa3qqh6ynMuWBzS-2BtF926Ro0DMRGPJM9uOxh5-2FjTuh75EyQgEjxKKgzuHLEm1aHjh6q0A-2BtSotGpKlPpXXH1aEB3rlw-3D-3D" alt="" width="1" height="1" border="0" style="height:1px !important;width:1px !important;border-width:0 !important;margin-top:0 !important;margin-bottom:0 !important;margin-right:0 !important;margin-left:0 !important;padding-top:0 !important;padding-bottom:0 !important;padding-right:0 !important;padding-left:0 !important;"/>
+    </body>
+</html>
